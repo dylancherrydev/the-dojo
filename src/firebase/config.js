0c0d96c290ab672f5cd511a58cf48ea3ec22f657
Dylan Cherry <dylancherry98@gmail.com>
2022-11-03 08:23:34 -0400

    setup firebase inside repo files

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbZGwq8YYPF7otgUcYv4wExRrz2RSUiMo",
+  authDomain: "thedojosite-94989.firebaseapp.com",
+  projectId: "thedojosite-94989",
+  storageBucket: "thedojosite-94989.appspot.com",
+  messagingSenderId: "327682614060",
+  appId: "1:327682614060:web:0e824a8a0d88ce6c9a0289"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+
+export { projectFirestore, projectAuth, timestamp }
