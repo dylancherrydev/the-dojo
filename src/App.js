d7695414516005d334c262f33225c13787110ce8
Dylan Cherry <dylancherry98@gmail.com>
2022-11-03 08:56:50 -0400

    set up router

--- src/App.js
+++ src/App.js
@@ -13,7 +13,27 @@ import Project from './pages/project/Project'
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
