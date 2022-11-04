d48800fc2dc5e3f41766a87d2cf9a1ddba785f90
Dylan Cherry <dylancherry98@gmail.com>
2022-11-04 09:51:02 -0400

    Added misisng 'exact' for navbar link in Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,7 +16,7 @@ export default function Sidebar() {
         <nav className="links">
           <ul>
             <li>
-              <NavLink to="/">
+              <NavLink exact to="/">
                 <img src={DashboardIcon} alt="dashboard icon" />
                 <span>Dashboard</span>
               </NavLink>
