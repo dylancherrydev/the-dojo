36703bf765eb8cfce142bc9bc887fc44fddf5b8d
Dylan Cherry <dylancherry98@gmail.com>
2022-11-06 16:25:10 -0500

    added firebase storage to firebase config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBbZGwq8YYPF7otgUcYv4wExRrz2RSUiMo",
@@ -17,9 +18,10 @@ firebase.initializeApp(firebaseConfig)
 // init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp
 
 
-export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, projectStorage, timestamp }
