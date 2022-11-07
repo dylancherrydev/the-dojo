54577c0a399be5501d5713d617546de24ca8cf16
Dylan Cherry <dylancherry98@gmail.com>
2022-11-07 08:35:51 -0500

    designed Login form, need to hook it up with firebase via useLogin hook

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,42 @@
+import { useState } from 'react'
+
+// styles
 import './Login.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email" 
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password" 
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button>Login</button>
+      {/* {!isPending && <button className="btn">Login</button>}
+      {isPending && <p className='btn' disabled>Loading...</p>}
+      {error && <p className='error'>{error}</p>} */}
+  </form>
   )
 }
