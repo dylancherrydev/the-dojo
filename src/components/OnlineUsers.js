c4ecdb37c5a67294660a572e21dcb486ed63e18b
Dylan Cherry <dylancherry98@gmail.com>
2022-11-08 08:47:11 -0500

    made OnlineUsers component show list of user displaynames and avatars

---
+++ src/components/OnlineUsers.js
@@ -0,0 +1,23 @@
+import { useCollection } from '../hooks/useCollection.js'
+import Avatar from './Avatar.js'
+
+// styles
+import "./OnlineUsers.css"
+
+
+export default function OnlineUsers() {
+  const { documents, error } = useCollection('users')
+
+  return (
+    <div className='user-list'>
+      <h2>All Users:</h2>
+      {error && <div className='error'>{error}</div>}
+      {documents && documents.map(user => (
+        <div key={user.id}>
+          <span>{user.displayName}</span>
+          <Avatar src={user.photoURL} />          
+        </div>
+      ))}
+    </div>
+  )
+}
