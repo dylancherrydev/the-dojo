cc7725fc5e0294f5f74fa02c17fcf19a88e3aee4
Dylan Cherry <dylancherry98@gmail.com>
2022-11-08 08:34:12 -0500

    sidebar now displays user displayName & avatar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom'
+import Avatar from './Avatar'
+import { useAuthContext } from "../hooks/useAuthContext"
 
 // styles & images
 import './Sidebar.css'
@@ -6,12 +8,16 @@ import DashboardIcon from '../assets/dashboard_icon.svg'
 import AddIcon from "../assets/add_icon.svg"
 
 export default function Sidebar() {
+  const { user } = useAuthContext()
+
   return (
     <div className='sidebar'>
       <div className="sidebar-content">
         <div className="user">
-          {/* avatar and username here later */}
-          <p>Hey, user</p>
+
+          <p>Hey, {user.displayName}</p>
+          <Avatar src={user.photoURL}/>
+          
         </div>
         <nav className="links">
           <ul>
