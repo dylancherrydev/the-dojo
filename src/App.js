96a915c914ee2a35f433952d20f186510f8f641f
Dylan Cherry <dylancherry98@gmail.com>
2022-11-08 09:00:14 -0500

    added green dot next to online users in user list

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Signup from './pages/signup/Signup'
 import Project from './pages/project/Project'
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import OnlineUsers from './components/OnlineUsers';
 
 function App() {
   const { user, authIsReady } = useAuthContext()
@@ -46,6 +47,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       ]} 
     </div>
--- src/components/OnlineUsers.js
+++ src/components/OnlineUsers.js
@@ -14,6 +14,7 @@ export default function OnlineUsers() {
       {error && <div className='error'>{error}</div>}
       {documents && documents.map(user => (
         <div key={user.id} className="user-list-item">
+          {user.online && <span className='online-user'></span>}
           <span>{user.displayName}</span>
           <Avatar src={user.photoURL} />          
         </div>
