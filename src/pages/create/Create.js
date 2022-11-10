97ef99291e19e19917f45a9f56e986bff1535df9
Dylan Cherry <dylancherry98@gmail.com>
2022-11-10 07:00:54 -0500

    started filling out Create page, added state

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,19 @@
 import './Create.css'
+import { useState } from 'react'
 
 export default function Create() {
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState (' ')
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers]= useState([])
+
   return (
-    <div>Create</div>
+    <div className='create-from'>
+      <h2 className="page-title">Create a new project</h2>
+      <form>
+
+      </form>
+    </div>
   )
 }
