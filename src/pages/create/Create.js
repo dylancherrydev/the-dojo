de1c91471f821a595488287183128c43d40fc7e1
Dylan Cherry <dylancherry98@gmail.com>
2022-11-10 19:25:20 -0500

    Completed Create page to work with firestore db

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,8 +1,10 @@
+import { useState, useEffect } from 'react'
 import { useCollection } from '../../hooks/useCollection'
-import { useEffect, useState } from 'react'
-import Select from 'react-select'
-import { timestamp } from '../../firebase/config'
 import { useAuthContext } from '../../hooks/useAuthContext'
+import { timestamp } from '../../firebase/config'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useHistory } from 'react-router-dom'
+import Select from 'react-select'
 
 // styles
 import './Create.css'
@@ -15,96 +17,99 @@ const categories = [
 ]
 
 export default function Create() {
+  const history = useHistory()
+  const { addDocument, response } = useFirestore('projects')
+  const { user } = useAuthContext()
   const { documents } = useCollection('users')
   const [users, setUsers] = useState([])
-  const { user } = useAuthContext()
-
-  useEffect(() => {
-      if(documents) {
-        const options = documents.map(user => {
-          return {value: user, label: user.displayName }
-        })
-        setUsers(options)
-      }
-  },[documents])
 
   // form field values
   const [name, setName] = useState('')
   const [details, setDetails] = useState('')
-  const [dueDate, setDueDate] = useState (' ')
+  const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
-  const [assignedUsers, setAssignedUsers]= useState([])
+  const [assignedUsers, setAssignedUsers] = useState([])
   const [formError, setFormError] = useState(null)
 
-  const handleSubmit = (e) => {
+  // create user values for react-select
+  useEffect(() => {
+    if(documents) {
+      setUsers(documents.map(user => {
+        return { value: {...user, id: user.id}, label: user.displayName }
+      }))
+    }
+  }, [documents])
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
     setFormError(null)
 
     if (!category) {
-      setFormError('Please select a project category')
+      setFormError('Please select a project category.')
       return
     }
     if (assignedUsers.length < 1) {
-      setFormError('Please assign the project to at least one user')
+      setFormError('Please assign the project to at least 1 user')
       return
     }
 
-    const createdBy = {
-      displayName: user.displayName,
-      photoURL: user.photoUrl,
-      id: user.uid,
-    }
-
-    const assignedUsersList = assignedUsers.map((u) => {
-      return {
-        displayName: u.value.displayName,
+    const assignedUsersList = assignedUsers.map(u => {
+      return { 
+        displayName: u.value.displayName, 
         photoURL: u.value.photoURL,
-        id: u.value.id,
+        id: u.value.id
       }
     })
+    const createdBy = { 
+      displayName: user.displayName, 
+      photoURL: user.photoURL,
+      id: user.uid
+    }
 
     const project = {
       name,
       details,
+      assignedUsersList, 
+      createdBy,
       category: category.value,
       dueDate: timestamp.fromDate(new Date(dueDate)),
-      comments: [],
-      createdBy,
-      assignedUsersList,
+      comments: []
     }
 
-    console.log(project)
+    await addDocument(project)
+    if (!response.error) {
+      history.push('/')
+    }
   }
 
   return (
-    <div className='create-from'>
-      <h2 className="page-title">Create a new project</h2>
+    <div className="create-form">
+      <h2 className="page-title">Create a new Project</h2>
       <form onSubmit={handleSubmit}>
         <label>
           <span>Project name:</span>
-          <input 
-            type="text"
+          <input
+            required 
+            type="text" 
             onChange={(e) => setName(e.target.value)}
-            required
-            value={name} 
+            value={name}
           />
         </label>
         <label>
-          <span>Project details:</span>
-          <textarea
-            type="text"
-            onChange={(e) => setDetails(e.target.value)}
+          <span>Project Details:</span>
+          <textarea 
             required
+            onChange={(e) => setDetails(e.target.value)}
             value={details} 
           ></textarea>
         </label>
         <label>
           <span>Set due date:</span>
-          <input 
-            type="date"
-            onChange={(e) => setDueDate(e.target.value)}
-            required
-            value={dueDate} 
+          <input
+            required 
+            type="date" 
+            onChange={(e) => setDueDate(e.target.value)} 
+            value={dueDate}
           />
         </label>
         <label>
@@ -116,14 +121,16 @@ export default function Create() {
         </label>
         <label>
           <span>Assign to:</span>
-          <Select 
+          <Select
             onChange={(option) => setAssignedUsers(option)}
             options={users}
             isMulti
           />
         </label>
-        <button className="btn">Add project</button>
-        {formError && <p className='error'>{formError}</p>}
+
+        <button className="btn">Add Project</button>
+
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   )
