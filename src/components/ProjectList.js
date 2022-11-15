49153b3cafc111a46109ba9a511e49c873a925c8
Dylan Cherry <dylancherry98@gmail.com>
2022-11-15 08:46:00 -0500

    added ProjectList styling

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,7 @@ import "./ProjectList.css"
 
 export default function ProjectList( {projects} ) {
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p>No projects yet</p>}
       {projects.length > 0 && projects.map(project => (
         <Link to={`/projects/${project.id}`} key={project.id}>
