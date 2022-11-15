32cf1318a504c23dce4cd29197688c551e359ca5
Dylan Cherry <dylancherry98@gmail.com>
2022-11-15 08:39:50 -0500

    built backend logic for ProjectList.js, need to add styling

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom"
+import Avatar from "./Avatar"
+
 // styles
 import "./ProjectList.css"
 
@@ -6,7 +9,19 @@ export default function ProjectList( {projects} ) {
     <div>
       {projects.length === 0 && <p>No projects yet</p>}
       {projects.length > 0 && projects.map(project => (
-        <p key={project.id}>{project.name}</p>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map(user => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL}/>
+                </li>
+              ))}
+            </ul>  
+          </div>
+        </Link>
       ))}  
     </div>
   )
