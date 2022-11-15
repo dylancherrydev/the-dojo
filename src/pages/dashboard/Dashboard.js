a7c0b25d875c1daa525c059359338ce6481c86fb
Dylan Cherry <dylancherry98@gmail.com>
2022-11-15 08:15:54 -0500

    implememnted useCollection in Dashboard, started building the dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,7 +1,18 @@
+import { useCollection } from '../../hooks/useCollection'
+
+// styles
 import './Dashboard.css'
 
 export default function Dashboard() {
+  const { documents, error } = useCollection('projects')
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && documents.map(doc => (
+        <p key={doc.id}>{doc.name}</p>
+      ))}
+    </div>
   )
 }
