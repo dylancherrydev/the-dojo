6d39558dcc65f6e06294095f954a6823126c012a
Dylan Cherry <dylancherry98@gmail.com>
2022-11-23 08:18:23 -0500

    created ProjectComments component

---
+++ src/pages/project/ProjectComments.js
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { timestamp } from "../../firebase/config"
+import { useAuthContext } from "../../hooks/useAuthContext"
+
+export default function ProjectComments() {
+  const [newComment, setNewComment] = useState('')
+  const { user } = useAuthContext()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const commentToAdd = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      content: newComment,
+      createdAt: timestamp.fromDate(new Date()),
+      id: Math.random()
+    }
+    console.log(commentToAdd)
+  }
+
+  return (
+    <div className="project-comments">
+      <h4>Project Comments:</h4>
+
+      <form onSubmit={handleSubmit} className="add-comment">
+        <label>
+          <span>Add new comment:</span>
+          <textarea
+            required
+            onChange={(e) => setNewComment(e.target.value)}
+            value={newComment}
+          ></textarea>
+        </label>
+        <button className="btn"Add comment></button>
+      </form>
+    </div>
+  )
+}
