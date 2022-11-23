5c4b59e8dfa0c69e64f77c877ebdb0d3dafb1569
Dylan Cherry <dylancherry98@gmail.com>
2022-11-23 08:33:46 -0500

    updated useFirestore to be able to update documents

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -14,6 +14,8 @@ const firestoreReducer = (state, action) => {
       return { isPending: true, document: null, success: false, error: null }
     case 'ADDED_DOCUMENT':
       return { isPending: false, document: action.payload, success: true, error: null }
+    case 'UPDATED_DOCUMENT':
+      return { isPending: false, document: action.payload, success: true, error: null }
     case 'DELETED_DOCUMENT':
       return { isPending: false, document: null, success: true, error: null }
     case 'ERROR':
@@ -64,10 +66,24 @@ export const useFirestore = (collection) => {
     }
   }
 
+  // update document
+  const updateDocument = async (id, updates) => {
+    dispatch({ type: 'IS_PENDING' }) 
+
+    try {
+      const updatedDocument = await ref.doc(id).update(updates)
+      dispatchIfNotCancelled({ type: 'UPDATED_DOCUMENT', payload: updatedDocument })
+      return updatedDocument
+    }
+    catch(err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
+    }
+  }
+
   useEffect(() => {
     return () => setIsCancelled(true)
   }, [])
 
-  return { addDocument, deleteDocument, response }
+  return { addDocument, deleteDocument, updateDocument, response }
 
 }
